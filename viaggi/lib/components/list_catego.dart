5be19b646593737f6c6c5383c5c2d023552511c8
andre_imbro <imbrosciano19@gmail.com>
2022-05-05 00:42:40 +0200

    viaggi

--- viaggi/lib/components/list_catego.dart
+++ viaggi/lib/components/list_catego.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:viaggi/models/interessi.dart';
 
-class ListCat extends StatelessWidget {
+class ListCat extends StatefulWidget {
   const ListCat({Key? key}) : super(key: key);
 
+  @override
+  State<ListCat> createState() => _ListCatState();
+}
+
+class _ListCatState extends State<ListCat> {
+  bool select = false;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -14,38 +20,47 @@ class ListCat extends StatelessWidget {
             itemCount: Interessi.values.length,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.all(10),
-                margin: index != Interessi.values.length - 1
-                    ? const EdgeInsets.only(right: 20, top: 10, bottom: 10)
-                    : const EdgeInsets.only(top: 10, bottom: 10),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black12),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    InkWell(
-                      child: Container(
+              return InkWell(
+                onTap: (() {
+                  setState(() {
+                    select = !select;
+                  });
+                }),
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  margin: index != Interessi.values.length - 1
+                      ? const EdgeInsets.only(right: 20, top: 10, bottom: 10)
+                      : const EdgeInsets.only(top: 10, bottom: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
                         margin: const EdgeInsets.only(bottom: 10),
                         padding: const EdgeInsets.all(10),
                         decoration: BoxDecoration(
-                            color: Interessi.values[index].color,
+                            color: select
+                                ? Colors.white
+                                : Interessi.values[index].color,
                             borderRadius: BorderRadius.circular(12)),
                         child: Icon(
                           Interessi.values[index].icon,
-                          color: Colors.white,
+                          color: select
+                              ? Interessi.values[index].color
+                              : Colors.white,
                         ),
                       ),
-                    ),
-                    Text(
-                      Interessi.values[index].name,
-                      style: const TextStyle(
-                          fontSize: 14, fontWeight: FontWeight.w400),
-                    )
-                  ],
+                      Text(
+                        Interessi.values[index].name,
+                        style: const TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w400),
+                      )
+                    ],
+                  ),
                 ),
               );
             }),
