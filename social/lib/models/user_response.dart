17d35fc067003a2ea628f1981c5fa8d60a4499aa
andre_imbro <imbrosciano19@gmail.com>
2022-05-04 10:57:25 +0200

    viaggi

---
+++ social/lib/models/user_response.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'user.dart';
+
+part 'user_response.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class UserResponse {
+  final List<User> data;
+  final int total;
+  final int page;
+  final int limit;
+
+  const UserResponse(
+      {required this.data,
+      required this.total,
+      required this.limit,
+      required this.page});
+
+  factory UserResponse.fromJson(Map<String, dynamic> json) =>
+      _$UserResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserResponseToJson(this);
+}
